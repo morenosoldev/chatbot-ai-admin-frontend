bbfda37caf4239da4a63c9a26b7bb79fe622c64d
Dani025a <dani025a@edu.easj.dk>
2023-12-24 08:38:24 +0100

    Fixed some issues

--- cypress.config.js
+++ cypress.config.js
@@ -9,8 +9,15 @@ export default defineConfig({
   video: false,
 
   e2e: {
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
     },
   },
 });
