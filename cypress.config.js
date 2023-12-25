85498b4c3825eb9e959b6129fc92ca84d24c1ee3
Andreas Moreno Jønsson <andreas.ecuador@live.dk>
2023-12-25 20:55:53 +0100

    ny base url

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   video: false,
 
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:4173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
